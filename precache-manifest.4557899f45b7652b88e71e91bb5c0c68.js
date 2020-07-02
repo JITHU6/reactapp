9c1b297d0167f8cd717a389c85cd273c3458b457
Jithu <jithu.benny@innovaturelabs.org>
2020-07-02 18:08:45 +0530

    Updates

---
+++ precache-manifest.4557899f45b7652b88e71e91bb5c0c68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b066e52bd65dffa972c2b14828416c1",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "c7858a639ec5b9279519",
+    "url": "/reactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ec00c0238fe7e7197ce4",
+    "url": "/reactapp/static/js/2.796a4abe.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/reactapp/static/js/2.796a4abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7858a639ec5b9279519",
+    "url": "/reactapp/static/js/main.27d454b2.chunk.js"
+  },
+  {
+    "revision": "92934c2542537aaaf095",
+    "url": "/reactapp/static/js/runtime-main.ee9db8f9.js"
+  }
+]);
